f49c909dc2a44b452f6e13d34c4ff8b0eca2424a
mwhitfieldwork <mwhitfieldwork@gmail.com>
2024-09-28 20:18:51 -0500

    Start the routing

--- nwrevised/src/app/app.routes.ts
+++ nwrevised/src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import {ProductTableComponent} from "./northwind-ui/products/product-table/product-table.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', component: ProductTableComponent}
+]

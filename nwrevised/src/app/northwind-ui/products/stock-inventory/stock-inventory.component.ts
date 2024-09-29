2f6de9da4657694334fb702cb4a269c580f384e5
mwhitfieldwork <mwhitfieldwork@gmail.com>
2024-09-28 19:41:35 -0500

    Start revised structure for project

---
+++ nwrevised/src/app/northwind-ui/products/stock-inventory/stock-inventory.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-stock-inventory',
+  standalone: true,
+  imports: [],
+  templateUrl: './stock-inventory.component.html',
+  styleUrl: './stock-inventory.component.scss'
+})
+export class StockInventoryComponent {
+
+}

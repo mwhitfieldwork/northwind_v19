f62d1c76d1983cb94cf7a83d1aa8c2f22dedf6de
mwhitfieldwork <mwhitfieldwork@gmail.com>
2025-03-12 17:42:28 -0500

    Wire up stock page

--- nwrevised/src/app/northwind-ui/products/stock-inventory/stock-inventory.component.ts
+++ nwrevised/src/app/northwind-ui/products/stock-inventory/stock-inventory.component.ts
@@ -1,12 +1,75 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, FormArray, ReactiveFormsModule, FormBuilder} from '@angular/forms';
+import { StockBranchComponent } from "./stock-branch/stock-branch.component";
+import {StockSelectorComponent} from "./stock-selector/stock-selector.component"
+import { StockProductsComponent } from "./stock-products/stock-products.component";
+import { JsonPipe } from '@angular/common';
+import {ProductModel} from "../../../utilities/models/product"
 
 @Component({
   selector: 'app-stock-inventory',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule, 
+    StockBranchComponent, 
+    StockSelectorComponent, 
+    StockProductsComponent,
+    JsonPipe ],
   templateUrl: './stock-inventory.component.html',
   styleUrl: './stock-inventory.component.scss'
 })
-export class StockInventoryComponent {
 
+//Creating a small feature that allows product to be ordered
+export class StockInventoryComponent {
+  products: ProductModel[] =[
+    {categoryId:2,
+      unitPrice: 20,
+      productName:"test"
+    },
+    {categoryId:3,
+      unitPrice: 120,
+      productName:"test two"
+    },
+    {categoryId:4,
+      unitPrice: 230,
+      productName:"test three"
+    },
+  ]
+
+  form = new FormGroup({
+    store: new FormGroup({ //passed down to the stock branch component
+      branch: new FormControl('B182'),
+      code: new FormControl('1234'),
+    }),
+    selector: new FormGroup({ //passed to the selector component
+      product_id: new FormControl(),
+      quantity: new FormControl(10)
+    }),
+    stock: new FormArray([
+      this.createStock({product_id:1, quantity:60}),
+      this.createStock({product_id:3, quantity:30})
+    ])//collection of form controls and form groups to be passed to stock-products
+  })
+
+  
+  createStock(stock:any){
+    return new FormGroup({ //passed to the selector component
+      product_id: new FormControl(stock.product_id || ''),
+      quantity: new FormControl(stock.quantity || 10)
+    })
+  }
+
+  addStock(value:any){
+    console.log(value, 'Value')
+    const control = this.form.get('stock') as FormArray;
+    control.push(this.createStock(value));
+  }
+
+  removeStock({group,index}:{group:FormGroup,index:number}){
+    const control = this.form.get('stock') as FormArray;
+    control.removeAt(index);
+    console.log(group, index) 
+  }
+  onSubmit(){
+    console.log('Submit', this.form.value)
+  }
 }
--- nwrevised/src/app/northwind-ui/products/stock-inventory/stock-selector/stock-selector.component.ts
+++ nwrevised/src/app/northwind-ui/products/stock-inventory/stock-selector/stock-selector.component.ts
@@ -1,12 +1,33 @@
-import { Component } from '@angular/core';
-
+import { Component, EventEmitter, Input, input, Output } from '@angular/core';
+import { FormGroup, FormControl, ReactiveFormsModule } from '@angular/forms';
+import { ProductModel } from '../../../../utilities/models/product';
 @Component({
-  selector: 'app-stock-selector',
+  selector: 'stock-selector',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './stock-selector.component.html',
   styleUrl: './stock-selector.component.scss'
 })
 export class StockSelectorComponent {
+  @Input() parent!: FormGroup //switch to signal
+  @Input() products!:ProductModel[];
+  @Output() added = new EventEmitter<any>();
 
+  onAdd(){
+    console.log(this.parent.get('selector')?.value, 'Selector')
+    this.added.emit(this.parent.get('selector')?.value);
+    this.parent.get('selector')?.reset({// reset changes the dom back to pristine, where set and patch value wont
+      product_id:'',
+      quantity:10,
+      name:''
+    });
+    
+    this.parent.get('selector')?.setValue({ //updates mulitple controls at once, must have the keyand the value
+      product_id:'',
+      quantity:10
+    });
+    this.parent.get('selector')?.patchValue({//used to update a single control
+      product_id:''
+    });
+  }
 }

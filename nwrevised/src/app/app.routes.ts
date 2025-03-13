dc51a05ea8c61bac933f7d7f1473cb325c900234
mwhitfieldwork <mwhitfieldwork@gmail.com>
2025-03-13 13:11:20 -0500

    Add navigation and logo to project

--- nwrevised/src/app/app.routes.ts
+++ nwrevised/src/app/app.routes.ts
@@ -1,8 +1,11 @@
 import { Routes } from '@angular/router';
 import {ProductTableComponent} from "./northwind-ui/products/product-table/product-table.component";
 import { StockInventoryComponent } from './northwind-ui/products/stock-inventory/stock-inventory.component';
+import { DashboardComponent } from './northwind-ui/dashboard/dashboard.component';
 
 export const routes: Routes = [
-    {path: '', component: ProductTableComponent},
+    {path: '', component:DashboardComponent },
+    {path: 'dashboard', component:DashboardComponent },
     {path: 'stock', component: StockInventoryComponent},
+    {path: 'products', component: ProductTableComponent},
 ]

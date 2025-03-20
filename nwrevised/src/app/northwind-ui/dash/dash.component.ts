691e8bcc10334101b0152b4c60efd15ef9357200
mwhitfieldwork <mwhitfieldwork@gmail.com>
2025-03-20 13:40:20 -0500

    Adjust styles to allow for scrolling and use shared component on dashboard

--- nwrevised/src/app/northwind-ui/dash/dash.component.ts
+++ nwrevised/src/app/northwind-ui/dash/dash.component.ts
@@ -1,15 +1,17 @@
 import { Component } from '@angular/core';
 import { ProductTableComponent } from "../products/product-table/product-table.component";
 import { EmployeesComponent } from "../employees/employees.component";
+import { CardComponent } from "../../shared/card/card.component";
 
 @Component({
   selector: 'app-dash',
   standalone: true,
-  imports: [ProductTableComponent, EmployeesComponent],
+  imports: [ProductTableComponent, EmployeesComponent, CardComponent],
   templateUrl: './dash.component.html',
   styleUrl: './dash.component.scss'
 })
 export class DashComponent {
-
+  totalOrders:number = 0;
+  averageOrderPrice:number = 0;
 
 }

bc9f0620fbd1a57fa443393c646a0694f7907429
mwhitfieldwork <mwhitfieldwork@gmail.com>
2025-04-08 12:58:37 -0500

    Add content projected buttons to login page

---
+++ nwrevised/src/app/shared/basic-button/basic-button.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: '[appButton]', //not limited tot he tag selector 
+  standalone: true,
+  imports: [],
+  templateUrl: './basic-button.component.html',
+  styleUrl: './basic-button.component.scss'
+})
+export class BasicButtonComponent {
+
+}

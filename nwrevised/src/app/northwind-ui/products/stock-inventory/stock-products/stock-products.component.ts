f742e7c06cdee41d44059c91233d6240158851d4
mwhitfieldwork <mwhitfieldwork@gmail.com>
2025-04-09 15:18:57 -0500

    Add shared component of rating and clean other things

--- nwrevised/src/app/northwind-ui/products/stock-inventory/stock-products/stock-products.component.ts
+++ nwrevised/src/app/northwind-ui/products/stock-inventory/stock-products/stock-products.component.ts
@@ -1,22 +1,20 @@
 import { DatePipe } from '@angular/common';
-import { Component, Input, OnInit, Output, EventEmitter, computed, input } from '@angular/core';
+import { Component, Input, Output, EventEmitter, computed, input } from '@angular/core';
 import { FormArray, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { RatingComponent } from '../../../../shared/rating/rating.component';
 
 @Component({
   selector: 'stock-products',
   standalone: true,
-  imports: [ReactiveFormsModule, DatePipe],
+  imports: [ReactiveFormsModule, DatePipe, RatingComponent],
   templateUrl: './stock-products.component.html',
   styleUrl: './stock-products.component.scss'
 })
-export class StockProductsComponent implements OnInit{
+export class StockProductsComponent{
   parent = input<FormGroup>(new FormGroup({})); 
   @Output() removed = new EventEmitter<any>();
   stocks = computed(() => (this.parent().get('stock') as FormArray).controls);
 
-  ngOnInit(): void {
-    //console.log(this.stocks)
-  }
 
   onRemove(group:any, index:number){
     this.removed.emit({group, index})

1af83b633a65a910e24463b69fef77f34d155cac
mwhitfieldwork <mwhitfieldwork@gmail.com>
2025-04-08 20:54:18 -0500

    Add route guard to the calculate path only admin can see

--- nwrevised/src/app/utilities/services/user-session/user-session.service.ts
+++ nwrevised/src/app/utilities/services/user-session/user-session.service.ts
@@ -1,17 +1,24 @@
 import { inject, Injectable, signal } from '@angular/core';
 import { Authentication } from '../../models/authentication';
-import { catchError, Observable, tap, throwError } from 'rxjs';
+import { BehaviorSubject, catchError, Observable, tap, throwError } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
-
+import { User } from '../../models/user.model'
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserSessionService {
   private _http = inject(HttpClient);
+  private userSubject = new BehaviorSubject<User | null>(null);
+  public user$: Observable<User | null> = this.userSubject.asObservable();
 
   url:string = 'https://localhost:7216';
   errorMessage:any;
+  
+  public get currentUser(): User | null {
+    console.log(this.userSubject.getValue(), "User Gotten")
+    return this.userSubject.getValue();
+  }
 
   setUser(id:string): void {
       localStorage.setItem('user', JSON.stringify(id)); 
@@ -22,14 +29,14 @@ export class UserSessionService {
     var response = this._http.get<Authentication>(url)
       .pipe(
         tap(item => {
-          //console.log(item)
+          console.log(item, "This is the USER!!")
         }),
         catchError(this.handleError),
       )
 
     return response
   }
-
+  
   private handleError(error: Response) {
     console.error(error);
     return throwError(() => error || 'Server error');
